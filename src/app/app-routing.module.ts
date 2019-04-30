c0c37f55e883f52132f3b04c48a44c3aaba896ad
i.vivdich <i.vivdich@outlook.com>
2019-04-30 18:40:14 +0300

    login/header initial work/rework

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'header', component: HeaderComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '', component: AppComponent },
+  { path: '**', component: AppComponent } // temporary
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
